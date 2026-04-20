9c6ca50e639d8da1e9b2b8494b8c29985be6e0c2
TAG <jasonhsmith396@gmail.com>
2026-04-20 12:34:09 -0400

    Broadcast & functions set up

--- CU7/scripts/commandrep.js
+++ CU7/scripts/commandrep.js
@@ -2,4 +2,46 @@ import { world, ItemStack, system, Player } from "@minecraft/server";
 
 system.runInterval(() => {
     world.getDimension("overworld").runCommand("function worldborders");
+    world.getDimension("overworld").runCommand("function bot");
+    world.getDimension("overworld").runCommand("function cosmetics");
+    world.getDimension("overworld").runCommand("function illegalg");
+    const score = world.scoreboard.getObjective('score');
+    for (const player of world.getAllPlayers()) {
+        if ((player.hasTag('rank1')) || (player.hasTag('rank2')))
+        {
+            player.runCommand('function rank');
+        }
+    }
 });
+
+const messages = [
+    'tellraw @a[tag=broadcast] {"rawtext":[{"text":"[§bBroadcast§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a {"rawtext":[{"text":"[§6News§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=broadcast] {"rawtext":[{"text":"[§bBroadcast§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a {"rawtext":[{"text":"[§6News§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=broadcast] {"rawtext":[{"text":"[§bBroadcast§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a {"rawtext":[{"text":"[§cNotice§r] §bWelcome to Cubic Utopia 7!"}]}'
+];
+const tips = [
+    'tellraw @a[tag=tips] {"rawtext":[{"text":"[§eTip§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=tips] {"rawtext":[{"text":"[§eTip§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=tips] {"rawtext":[{"text":"[§eTip§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=tips] {"rawtext":[{"text":"[§eTip§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=tips] {"rawtext":[{"text":"[§eTip§r] §bWelcome to Cubic Utopia 7!"}]}',
+    'tellraw @a[tag=tips] {"rawtext":[{"text":"[§eTip§r] §bWelcome to Cubic Utopia 7!"}]}'
+];
+
+let messageIndex = 0;
+let tipIndex = 0;
+system.runInterval(() => {
+    if (messages.length > 0) {
+        world.getDimension("overworld").runCommand(messages[messageIndex]);
+        messageIndex = (messageIndex + 1) % messages.length;
+    }
+}, 7200);
+system.runInterval(() => {
+    if (tips.length > 0) {
+        world.getDimension("overworld").runCommand(tips[tipIndex]);
+        tipIndex = (tipIndex + 1) % tips.length;
+    }
+}, 9230);
